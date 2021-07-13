eb05661529e0a77a6c39956669fbf759dca17a8e
Blake Bourque <techplex.engineer@gmail.com>
2021-07-13 15:58:21 -0400

    Move the serve code out of the command

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,12 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 
 	"github.com/techplexengineer/luabyexample/tools"
@@ -29,47 +23,7 @@ on load to ensure up to date results are displayed.
 Note the static server only listens on localhost and should not 
 be used for production or network traffic.`,
 		Run: func(cmd *cobra.Command, args []string) {
-
-			r := mux.NewRouter()
-
-			dir := "./static"
-			dirEntries, err := os.ReadDir(dir)
-			if err != nil {
-				log.Print(fmt.Errorf("unable to list files in %s - %w", dir, err))
-			}
-			for _, entry := range dirEntries {
-				if entry.IsDir() {
-					continue
-				}
-				fileName := entry.Name()
-				r.HandleFunc("/"+fileName, func(w http.ResponseWriter, r *http.Request) {
-
-					log.Printf("Request for %s", "/"+fileName)
-					http.ServeFile(w, r, "./static/"+fileName)
-				})
-			}
-			r.HandleFunc("/{exampleId}", func(w http.ResponseWriter, r *http.Request) {
-
-				vars := mux.Vars(r)
-
-				example := tools.ParseExample(vars["exampleId"]) //@todo really need example name not id
-				example.PrevExample, example.NextExample = tools.GetPrevNextExample(vars["exampleId"])
-
-				tools.RenderExample(w, example)
-			})
-
-			r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-				examples := tools.GetExampleNamesAndIds("./examples.txt")
-				tools.RenderIndex(w, examples)
-			})
-
-			address := "127.0.0.1:" + port
-			fmt.Printf("Starting server on http://%s\n", address)
-			err = http.ListenAndServe(address, r)
-			if err != nil {
-				panic(err)
-			}
-
+			tools.ServeRebuild(port)
 		},
 	}
 
--- tools/serve.go
+++ tools/serve.go
@@ -1,11 +1,57 @@
 package tools
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
 	"net/http"
+	"os"
 )
 
 func ServeStatic(port string, rootDir string) {
 	err := http.ListenAndServe("127.0.0.1:"+port, http.FileServer(http.Dir(rootDir)))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ServeRebuild(port string) {
+	r := mux.NewRouter()
+
+	dir := "./static"
+	dirEntries, err := os.ReadDir(dir)
+	if err != nil {
+		log.Print(fmt.Errorf("unable to list files in %s - %w", dir, err))
+	}
+	for _, entry := range dirEntries {
+		if entry.IsDir() {
+			continue
+		}
+		fileName := entry.Name()
+		r.HandleFunc("/"+fileName, func(w http.ResponseWriter, r *http.Request) {
+
+			log.Printf("Request for %s", "/"+fileName)
+			http.ServeFile(w, r, "./static/"+fileName)
+		})
+	}
+	r.HandleFunc("/{exampleId}", func(w http.ResponseWriter, r *http.Request) {
+
+		vars := mux.Vars(r)
+
+		example := ParseExample(vars["exampleId"]) //@todo really need example name not id
+		example.PrevExample, example.NextExample = GetPrevNextExample(vars["exampleId"])
+
+		RenderExample(w, example)
+	})
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		examples := GetExampleNamesAndIds("./examples.txt")
+		RenderIndex(w, examples)
+	})
+
+	address := "127.0.0.1:" + port
+	fmt.Printf("Starting server on http://%s\n", address)
+	err = http.ListenAndServe(address, r)
 	if err != nil {
 		panic(err)
 	}
